2838928a09d0319e3a92fdd0fc78ec7f72916ce2
knee-cola <nikola.derezic@gmail.com>
2019-10-27 02:33:28 +0200

    building new version

---
+++ dist/lib/jest-mock-promise-types.d.ts
@@ -0,0 +1,12 @@
+declare enum PromiseState {
+    pending = 0,
+    resolved = 1,
+    rejected = 2,
+}
+declare type AnyFunction = (...args: any[]) => any;
+declare type HandlerType = {
+    then?: AnyFunction;
+    catch?: AnyFunction;
+    finally?: AnyFunction;
+};
+export { PromiseState, AnyFunction, HandlerType };
---
+++ dist/lib/jest-mock-promise.d.ts
@@ -0,0 +1,93 @@
+/**
+ * Synchronous Promise, which gets settled (resolved or settled) in a synchronous manner.
+ *
+ * `JestMockPromise` was written to simplify unit testing mocking (i.e. in [Jest](https://facebook.github.io/jest/) )
+ *
+ * In order to simplify synchronious promise settling two additional methods
+ * were added to the promise instance:
+ *   - `resolve` = forces the given promise to be resolved right away
+ *   - `reject` = forces the given promise to be rejected right away
+ *
+ * By using these methods, we can write something like (provided that the Promise is mocked):
+ *
+ *    let promise = ExternalComponent.doAyncWork();
+ *    promise.resolve({ label: 'this is some mock data' });
+ *
+ * @author   knee-cola<nikola.derezic@gmail.com>
+ * @license  @license MIT License, http://www.opensource.org/licenses/MIT
+ *
+ */
+import { AnyFunction } from './jest-mock-promise-types';
+declare class JestMockPromise {
+    private handlers;
+    private handlerIx;
+    private data;
+    private err;
+    private state;
+    constructor(callbackFn?: (x?: any, y?: any) => any);
+    /**
+     * Resolves the given promise
+     * @param value data which should be passed to `then` handler functions
+     */
+    private resolveFn(value?);
+    /**
+     * Rejects the given promise
+     * @param err error object which is to be passed as a param to `catch` function
+     */
+    private rejectFn(err);
+    /**
+     * Calls `finally` handlers
+     */
+    private callFinally();
+    /**
+     * Appends fulfillment and rejection handlers to the promise,
+     * and returns a new promise resolving to the return value of
+     * the called handler, or to its original settled value if the
+     * promise was not handled (i.e. if the relevant handler
+     * onFulfilled or onRejected is not a function).
+     * @param onFulfilled fulfillment handler function
+     * @param onRejected rejection handler function
+     */
+    then(onFulfilled: AnyFunction, onRejected?: AnyFunction): JestMockPromise;
+    /**
+     * Appends a rejection handler callback to the promise,
+     * and returns a new promise resolving to the return
+     * value of the callback if it is called, or to its
+     * original fulfillment value if the promise is instead
+     * fulfilled.
+     * @param onRejected rejection handler function
+     */
+    catch(onRejected: AnyFunction): this;
+    /**
+     * Appends a finally handler callback to the promise
+     * @param onFinally finally handler function
+     */
+    finally(onFinally: AnyFunction): this;
+    /**
+     * Resolves the promise with the given promise data.
+     * This is a non-standard method, which should be the last
+     * one to be called, after all the fulfillment and rejection
+     * handlers have been registered.
+     * @param {*} data
+     */
+    resolve(data?: any): void;
+    /**
+     * Rejects the promise with the given promise with the given error object.
+     * This is a non-standard method, which should be the last
+     * one to be called, after all the fulfillment and rejection
+     * handlers have been registered.
+     * @param {*} data
+     */
+    reject(err?: any): void;
+    /**
+     * Creates a resolved promise with the given data
+     * @param data data which should be passed to `then` handler functions
+     */
+    static resolve(data?: any): JestMockPromise;
+    /**
+     * Creates a rejected promise with the given data
+     * @param err error object which is to be passed as a param to `catch` function
+     */
+    static reject(err?: any): JestMockPromise;
+}
+export default JestMockPromise;
